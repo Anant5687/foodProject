ae9a51f902731fe914de51f7132f72c8fd4d267b
Anant Jindal <anantjindal45@gmail.com>
2022-05-14 12:50:17 +0530

    Project is finalised

--- src/components/CheckOut/Card.jsx
+++ src/components/CheckOut/Card.jsx
@@ -2,19 +2,23 @@ import React, { useState } from 'react'
 import style from './CheckOut.module.css'
 import { removeItem } from '../../counterSlice/orderSlice'
 import { useDispatch } from 'react-redux'
-import {addQuantity, removeQuantity} from '../../counterSlice/orderSlice'
+import { addQuantity, removeQuantity } from '../../counterSlice/orderSlice'
 
 const Card = ({ ele, index }) => {
   const dispatch = useDispatch()
-  
+
   const [alert, setAlert] = useState('')
 
 
 
   const addHandeler = (i) => {
-    dispatch(addQuantity(i))
+    if (ele.quantity >= 10) {
+      setAlert("You are not able to add more items")
+    } else {
+      dispatch(addQuantity(i))
+      setAlert('')
+    }
   }
-
 
   const fullRemoveHandeler = (x) => {
     dispatch(removeItem(x))
@@ -22,9 +26,12 @@ const Card = ({ ele, index }) => {
 
 
   const removeHandeler = (p) => {
-
-    dispatch(removeQuantity(p))
-
+    if (ele.quantity <= 1) {
+      setAlert("You are not able to delete more items")
+    } else {
+      dispatch(removeQuantity(p))
+      setAlert('')
+    }
   }
 
   return (
@@ -33,17 +40,35 @@ const Card = ({ ele, index }) => {
       <div className={style.card} >
         <div className={style.main}>
           <span>
+
             <img className={style.img} src={ele.url} />
+
             <h3 align="center" >MRP: Rs {ele.total()}</h3>
-            <button
-              onClick={()=>addHandeler(index)}
-              className={style.button} >Add</button>
-            <b className={style.value}>{ele.quantity}</b>
-            <button onClick={()=>removeHandeler(index)}
-              className={style.button2}>Remove</button>    <br />
-            <p style={{ color: "red" }}>{alert}</p><br /><br />
-            <button onClick={() => fullRemoveHandeler()} className={style.Remove_cart}>Remove from cart</button>
+
+            <button onClick={() => addHandeler(index)}
+              className={style.button} >
+              Add
+            </button>
+            <b className={style.value}>
+              {ele.quantity}
+            </b>
+            <button onClick={() => removeHandeler(index)}
+              className={style.button2}>
+              Remove
+            </button>
+            <br />
+            <p style={{ color: "red" }}>
+              {alert}
+            </p>
+            <br /><br />
+
+            <button onClick={() => fullRemoveHandeler()}
+              className={style.Remove_cart}>
+              Remove from cart
+            </button>
+
           </span>
+
         </div>
       </div>
     </>
